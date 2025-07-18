2b61793eae11b347118b407edf1e2a360b8bd8d9
Shlok Mishra <shlokmishra581@gmail.com>
2025-07-18 12:46:13 +0530

    Area of circle

---
+++ Area of circle.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+
+int main() {
+    float radius;
+    printf("Enter radius:");
+    scanf("%f", &radius);
+    printf("Area of circle is %f \n",3.14*radius*radius);
+    return 0;
+    
+}
