ba1bc3261c2083877b1fb9dd89407b255d948743
Shlok Mishra <shlokmishra581@gmail.com>
2025-07-18 12:05:31 +0530

    Outputs

---
+++ Outputs.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main() {
+    int age = 22;
+    float pi = 3.14;
+    char star = '*';
+    printf("Age is %d \n",age);
+    printf("Pi is %f \n",pi);
+    printf("Star is %c \n",star);
+    return 0;
+    
+}
