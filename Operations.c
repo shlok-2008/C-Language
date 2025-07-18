41ef3776433ac40fd727160bdb5a18d4a196ee8d
Shlok Mishra <shlokmishra581@gmail.com>
2025-07-18 12:38:45 +0530

    Operations

---
+++ Operations.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+
+int main() {
+    float a,b;
+    printf("Enter a:");
+    scanf("%f", &a);
+    printf("Enter b:");
+    scanf("%f", &b);
+    
+    printf("Sum is %f \n",a+b);
+    
+    printf("Sub is %f \n",a-b);
+    
+    printf("Multi is %f \n",a*b);
+    
+    printf("Div is %f \n",a/b);
+    return 0;
+    
+}
