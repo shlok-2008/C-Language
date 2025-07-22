6af22388c31cd0a3e8cfada75392c513e07a00cd
Shlok Mishra <shlokmishra581@gmail.com>
2025-07-22 11:13:03 +0530

    Avg of 3 nos

---
+++ Avg of 3 nos.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main() {
+    int a;
+    int b;
+    int c;
+    printf("Enter a=");
+    scanf("%d", &a);
+    printf("Enter b=");
+    scanf("%d", &b);
+    printf("Enter c=");
+    scanf("%d", &c);
+    printf("Average of 3 nos: %d", (a+b+c)/3);
+    
+    return 0;
+    
+}
