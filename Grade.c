e6d26f4716e8550f72c1a84a5fb70f02b91bd995
Shlok Mishra <shlokmishra581@gmail.com>
2025-07-31 11:49:37 +0530

    Create Grade.c

---
+++ Grade.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main() {
+    int marks;
+    printf("Enter marks:");
+    scanf("%d", &marks);
+    if (marks > 90) {
+        printf("Grade= A+");
+    }
+    else if (marks > 80 && marks <=90) {
+        printf("Grade= A");
+    }
+    else if (marks > 70 && marks <=80) {
+         printf("Grade= B+");
+    }
+    else if (marks > 60 && marks <=70) {
+        printf("Grade= B");
+    } 
+    else {
+        printf("Fail");
+    }        
+    return 0;
+    
+}
